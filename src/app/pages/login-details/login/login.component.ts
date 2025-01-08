797940b92d5ba7e601a63c7b44134e9f573d3b23
Hasindu Shehara <Hasindu.Shehara@INEIGHT.COM>
2025-01-08 19:42:55 +0530

    Created signup functionality

--- src/app/pages/login-details/login/login.component.ts
+++ src/app/pages/login-details/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { LoginService } from '../services/login.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +9,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
+  loginForm: any;
 
+  constructor(private router: Router, private loginService: LoginService) {
+    this.loginForm = new FormGroup({
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', Validators.required)
+    });
+  }
+
+  get Email() {
+    return this.loginForm.get('email');
+  }
+
+  get Password() {
+    return this.loginForm.get('password');
+  }
+
+  login() {
+    this.loginService.checkUserName(this.Email.value).subscribe((res: any) => {
+      if(res.code == 200 && res.message == 'User Exist') {
+        this.router.navigateByUrl("/dashboard")
+      }
+    })
+  }
 }
